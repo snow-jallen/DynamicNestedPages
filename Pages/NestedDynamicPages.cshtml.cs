17d7e8d50c66267a3e1614c19aa8912465bfe171
Jonathan Allen <jallen@snow.edu>
2021-11-30 10:14:25 -0700

    Add project files.

---
+++ Pages/NestedDynamicPages.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicNestedPages.Pages
+{
+    public class NestedDynamicPagesModel : PageModel
+    {
+        private readonly ILogger<NestedDynamicPagesModel> log;
+        private readonly ItemManager itemManager;
+
+        public NestedDynamicPagesModel(ILogger<NestedDynamicPagesModel> log, ItemManager itemManager)
+        {
+            this.log = log;
+            this.itemManager = itemManager;
+        }
+        public void OnGet(string parent, string child)
+        {
+            ParentName = parent;
+            ChildName = child;
+        }
+
+        [BindProperty]
+        public string ParentName { get; set; }
+        [BindProperty]
+        public string ChildName { get; set; }
+
+        public IActionResult OnPostAddParent()
+        {
+            log.LogInformation("Adding new parent: {parent}", ParentName);
+            itemManager.TopLevelItems.Add(new ParentItem { Name = ParentName });
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostAddChild()
+        {
+            log.LogInformation("Adding {child} as a child to {parent}", ChildName, ParentName);
+            itemManager.TopLevelItems.First(i => i.Name == ParentName).Children.Add(ChildName);
+            return RedirectToPage(new { parent = ParentName });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicNestedPages
+{
+    public class ParentItem
+    {
+        public string Name { get; set; }
+        public List<string> Children { get; set; } = new();
+    }
+
+    public class ItemManager
+    {
+        public List<ParentItem> TopLevelItems { get; set; } = new();
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddSingleton<ItemManager>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
